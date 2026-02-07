6906daa439e1343f2e481cd52401b5fed633690f
Babita <bs126416@gmail.com>
2026-02-07 10:54:13 +0530

    Initialize project using Create React App

---
+++ src/JsonFile.jsx
@@ -0,0 +1,15 @@
+export const JsonFile = () => {
+  return [
+    { id: 1, firstName: "Aarav", lastName: "Sharma", phone: "9876543210", email: "aarav1@gmail.com" },
+    { id: 2, firstName: "Neha", lastName: "Verma", phone: "9898989898", email: "neha2@gmail.com" },
+    { id: 3, firstName: "Rohit", lastName: "Kumar", phone: "9123456789", email: "rohit3@gmail.com" },
+    { id: 4, firstName: "Priya", lastName: "Singh", phone: "9012345678", email: "priya4@gmail.com" },
+    { id: 5, firstName: "Ankit", lastName: "Gupta", phone: "9988776655", email: "ankit5@gmail.com" },
+    { id: 6, firstName: "Pooja", lastName: "Malhotra", phone: "9876501234", email: "pooja6@gmail.com" },
+    { id: 7, firstName: "Rahul", lastName: "Mehta", phone: "9811122233", email: "rahul7@gmail.com" },
+    { id: 8, firstName: "Kavya", lastName: "Iyer", phone: "9822233344", email: "kavya8@gmail.com" },
+    { id: 9, firstName: "Vikas", lastName: "Chauhan", phone: "9833344455", email: "vikas9@gmail.com" },
+    { id: 10, firstName: "Simran", lastName: "Kaur", phone: "9844455566", email: "simran10@gmail.com" }
+
+  ];
+};
